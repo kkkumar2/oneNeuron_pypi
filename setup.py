594c2adf0b311ac6b3618d7960feb180afb793ef
MK <mk23.mohankumar@gmail.com>
2021-09-23 02:52:15 +0530

    Readme updated

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name=f"{PROJECT_NAME}-{USER_NAME}",
-    version="0.0.4",
+    version="0.0.5",
     author=USER_NAME,
     author_email="kmohankumar123456@gmail.com",
     description="This is a Single layer Perceptron package",
